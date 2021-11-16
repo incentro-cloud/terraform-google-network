a7129654bb3aa787a0d6cb8bf7d9247bd6a77deb
Robin Smorenburg <robin@smorenburg.io>
2021-11-16 17:13:11 +0100

    Add peerings features

--- modules/peerings/main.tf
+++ modules/peerings/main.tf
@@ -23,12 +23,16 @@ terraform {
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
-# NETWORK PEERINGS
+# PEERINGS
 # ---------------------------------------------------------------------------------------------------------------------
 
 resource "google_compute_network_peering" "peerings" {
-  for_each     = { for x in var.peerings : x.name => x }
-  name         = each.value.name
-  network      = each.value.network
-  peer_network = each.value.peer_network
+  for_each                            = { for x in var.peerings : x.name => x }
+  name                                = each.value.name
+  network                             = each.value.network
+  peer_network                        = each.value.peer_network
+  export_custom_routes                = each.value.export_custom_routes
+  import_custom_routes                = each.value.import_custom_routes
+  export_subnet_routes_with_public_ip = each.value.export_subnet_routes_with_public_ip
+  import_subnet_routes_with_public_ip = each.value.import_subnet_routes_with_public_ip
 }
