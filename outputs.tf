3e460d944f0ce75f884e52a23f755d84f6ba75e8
Robin Smorenburg <robin@smorenburg.io>
2021-12-30 09:39:04 +0100

    Modify outputs

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
-output "vpc" {
-  value       = var.create_vpc ? module.vpc.vpc[0] : null
+output "vpc_id" {
+  value       = var.create_vpc ? module.vpc.vpc[0].id : null
   description = "The VPC network."
 }
 
@@ -8,52 +8,27 @@ output "vpc_name" {
   description = "The name of the VPC network."
 }
 
-output "subnets" {
-  value       = module.subnets.subnets
-  description = "The subnets."
-}
-
 output "subnets_names" {
-  value       = { for subnet in module.subnets.subnets : subnet.name => lower(subnet.name) }
+  value       = { for subnet in module.subnets.subnets : subnet.name => subnet.name }
   description = "The names of the subnets."
 }
 
-output "routes" {
-  value       = module.routes.routes
-  description = "The routes."
-}
-
 output "routes_names" {
-  value       = [for route in module.routes.routes : route.name]
+  value       = { for route in module.routes.routes : route.name => route.name }
   description = "The names of the routes."
 }
 
-output "rules" {
-  value       = module.rules.rules
-  description = "The firewall rules."
-}
-
 output "rules_names" {
-  value       = [for rule in module.rules.rules : rule.name]
+  value       = { for rule in module.rules.rules : rule.name => rule.name }
   description = "The names of the firewall rules."
 }
 
-output "connectors" {
-  value       = module.connectors.connectors
-  description = "The connectors."
-}
-
 output "connectors_names" {
-  value       = [for connector in module.connectors.connectors : connector.name]
+  value       = { for connector in module.connectors.connectors : connector.name => connector.name }
   description = "The names of the connectors."
 }
 
-output "peerings" {
-  value       = module.peerings.peerings
-  description = "The peerings."
-}
-
 output "peerings_names" {
-  value       = [for peering in module.peerings.peerings : peering.name]
+  value       = { for peering in module.peerings.peerings : peering.name => peering.name }
   description = "The names of the peerings."
 }
